1051ade12b505577173f5c6a02d2bf42cbf286d0
Tony Dang <tonyzep966@outlook.com>
2022-11-16 11:21:07 +0800

    Initial commit for backend repo

---
+++ src/main/java/com/citrus64/simplesocialbackend/SimpleSocialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.citrus64.simplesocialbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSocialBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSocialBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/citrus64/simplesocialbackend/SimpleSocialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citrus64.simplesocialbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSocialBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
